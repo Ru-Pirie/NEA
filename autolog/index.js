a757dda3986a2343d63ba8070248127ae3dce1f5
Ru-Pirie <rubens.pirie@gmail.com>
2022-05-23 22:36:08 +0100

    Final commit of the night 23052022

---
+++ autolog/index.js
@@ -0,0 +1,42 @@
+const { google } = require("googleapis");
+
+async function main() {
+    const auth = new google.auth.GoogleAuth({
+        //the key file
+        keyFile: "creds.json", 
+        //url to spreadsheets API
+        scopes: "https://www.googleapis.com/auth/spreadsheets", 
+    });
+    
+    const authClientObject = await auth.getClient();
+    
+    const sheetID = '1M0x6IfIWKX9heKnwUo0QLy9aNsVWG9BsSVViUWelrtU';
+    const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+
+    await googleSheetsInstance.spreadsheets.values.update({
+        auth: authClientObject,
+        spreadsheetId: sheetID,
+        range: "NEA!C4",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [[new Date().toLocaleString()]]
+        },
+    })
+
+    await googleSheetsInstance.spreadsheets.values.append({
+        auth: authClientObject,
+        spreadsheetId: sheetID,
+        range: "NEA!A:D",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [["testid", "this is a test message", "> ", "https://google.com"]]
+        },
+    })
+}
+
+// https://api.github.com/repos/ru-pirie/NEA/git/refs/heads/main
+//
+
+
+
+main()
