0ad41b5be10b591752aa66a8d5f49a781ddf95c8
Rubens <rubens.pirie@gmail.com>
2022-05-24 10:47:50 +0000

    Added the logger

--- autolog/index.js
+++ autolog/index.js
@@ -1,9 +1,17 @@
 const { google } = require("googleapis");
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 async function main() {
     const res = await fetch('https://api.github.com/repos/ru-pirie/NEA/git/refs/heads/main')
+    const data = await res.json();
+    const lastCommitURL = data.object.url;
 
+    const commitRes = await fetch(`${lastCommitURL}`)
+    const commitData = await commitRes.json();
+    const lastCommitMSG = commitData.message
+    const lastCommitTime = new Date(commitData.committer.date).toLocaleString();
+  
     const auth = new google.auth.GoogleAuth({
         //the key file
         keyFile: "creds.json", 
@@ -22,24 +30,63 @@ async function main() {
         range: "NEA!C4",
         valueInputOption: "USER_ENTERED",
         resource: {
-            values: [[new Date().toLocaleString()]]
+            values: [[lastCommitTime]]
+        },
+    })
+      await googleSheetsInstance.spreadsheets.values.update({
+        auth: authClientObject,
+        spreadsheetId: sheetID,
+        range: "NEA!C5",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [[`${lastCommitMSG} (${commitData.sha.substring(0, 7)})`]]
         },
     })
 
-    await googleSheetsInstance.spreadsheets.values.append({
-        auth: authClientObject,
-        spreadsheetId: sheetID,
-        range: "NEA!A:D",
+    addCommit(googleSheetsInstance, authClientObject, sheetID, JSON.parse(fs.readFileSync('dummyPayload.json', 'UTF-8')), true)
+  
+    const express = require('express')
+    const app = express()
+    const port = 80
+
+    const bodyParser = require('body-parser')
+
+    app.use(bodyParser.json())
+
+  
+    app.post('/2f6c30ed-b48d-4f87-a263-8864fea42289', (req, res) => {
+      addCommit(googleSheetsInstance, authClientObject, sheetID, req.body)
+    })
+    
+    app.listen(port, () => {
+      console.log(`READY: ${port}`)
+    })
+
+    
+}
+
+async function addCommit(instance, auth, id, payload, val) {
+    const commit = payload.commits[0]
+    
+    await instance.spreadsheets.values.append({
+        auth: auth,
+        spreadsheetId: id,
+        range: "NEA!A:I",
         valueInputOption: "USER_ENTERED",
         resource: {
-            values: [["testid", "this is a test message", "> ", "https://google.com"]]
+            values: [[
+              commit.id.substring(0, 7),
+              val ? new Date().toLocaleString() : new Date(commit.timestamp).toLocaleString(),
+              `${commit.committer.name} (${commit.committer.email})`,
+              val ? `https://github.com/Ru-Pirie/NEA` : `https://github.com/Ru-Pirie/NEA/commit/${commit.id.substring(0, 7)}`,
+              commit.message,
+              commit.added.length !== 0 ? commit.added.join('\n') : "None",
+              commit.removed.length !== 0 ? commit.removed.join('\n') : "None",
+              commit.modified.length !== 0 ? commit.modified.join('\n') : "None",
+              val ? "" : "Teacher:\nStudent:"
+            ]]
         },
     })
 }
-
-// 
-//
-
-
 
 main()
