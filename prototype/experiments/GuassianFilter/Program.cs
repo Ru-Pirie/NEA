e859cedf170eeb1f0384e5d3261463ad25e3d5a3
Ru-Pirie <rubens.pirie@gmail.com>
2022-06-20 22:20:01 +0100

    commit for the night

--- prototype/experiments/GuassianFilter/Program.cs
+++ prototype/experiments/GuassianFilter/Program.cs
@@ -15,11 +15,18 @@ namespace GuassianFilter
                 for (int j = 0; j < k; j++)
                 {
                     kernel[i, j] = 1 / (2 * Math.PI * sigma * sigma) *
-                        Math.Exp(-((Math.Pow(i - (k + 1), 2) + Math.Pow(j - (k + 1), 2)) / (2 * sigma * sigma)));
+                        Math.Exp(-((Math.Pow(i, 2) + Math.Pow(j, 2)) / (2 * sigma * sigma)));
                 }
             }
 
-
+            for (int i = 0; i < kernel.GetLength(0); i++)
+            {
+                for (int j = 0; j < kernel.GetLength(1); j++)
+                {
+                    Console.Write(Math.Round(kernel[i, j], 2)+ " ");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
