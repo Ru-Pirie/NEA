572b6e7b5894caa2bf01aa250bf47633853d7142
Ru-Pirie <rubens.pirie@gmail.com>
2022-06-21 22:16:55 +0100

    Final commit of the night, next step gradient computation

--- prototype/experiments/IntensityGradient/Program.cs
+++ prototype/experiments/IntensityGradient/Program.cs
@@ -8,6 +8,9 @@ namespace IntensityGradient
 {
     internal class Program
     {
+        // step 2
+        // https://en.wikipedia.org/wiki/Sobel_operator
+        // https://en.wikipedia.org/wiki/Canny_edge_detector#Finding_the_intensity_gradient_of_the_image
         static void Main(string[] args)
         {
         }
