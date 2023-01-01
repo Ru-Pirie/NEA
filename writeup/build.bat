59480c3e053b62693cbe2e1b8f5b61c599156b3c
Rubens <rubens.pirie@gmail.com>
2023-01-01 18:12:03 +0000

    Started work on design chapter

--- writeup/build.bat
+++ writeup/build.bat
@@ -1,4 +1,25 @@
-DEL main.pdf
+@echo off
 
-pdflatex main.tex -shell-escape -interaction=nonstopmode
-pdflatex main.tex -shell-escape -interaction=nonstopmode
+echo Preparing to compile writeup...
+echo Utility script by Rubens Pirie
+
+DEL main.pdf
+echo Deleted old main.pdf
+
+if "%1" == "full" (
+  echo Running a full compilation of PDF this may take some time...
+  echo Pass 1
+  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+
+  echo Pass 2
+  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+
+) else (
+  echo Running a partial compilation of PDF run script with "full" as the first argument to run a full compilation.
+
+  echo Pass 1
+  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+
+)
+
+echo Done.
