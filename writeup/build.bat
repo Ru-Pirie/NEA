1bb13abbd70ceb5a2578fe3fb0c42c4b6ec09001
Rubens <rubens.pirie@gmail.com>
2023-01-16 09:47:29 +0000

    updated design

--- writeup/build.bat
+++ writeup/build.bat
@@ -3,24 +3,28 @@
 echo Preparing to compile writeup...
 echo Utility script by Rubens Pirie
 
-DEL main.pdf
-echo Deleted old main.pdf
-
-if "%1" == "full" (
-  echo Running a full compilation of PDF this may take some time...
-  echo Pass 1
-  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
-
-  echo Pass 2
-  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
-
+if  "%1" == "open" (
+    start chrome file://"%~dp0main.pdf"
 ) else (
-  echo Running a partial compilation of PDF run script with "full" as the first argument to run a full compilation.
+    DEL main.pdf
+    echo Deleted old main.pdf
 
-  echo Pass 1
-  pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+    if "%1" == "full" (
+    echo Running a full compilation of PDF this may take some time...
+    echo Pass 1
+    pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
 
+    echo Pass 2
+    pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+
+    ) else (
+    echo Running a partial compilation of PDF run script with "full" as the first argument to run a full compilation.
+
+    echo Pass 1
+    pdflatex main.tex -shell-escape -interaction=nonstopmode > nul
+
+    )
+
+    echo Done.
+    start chrome file://"%~dp0main.pdf"
 )
-
-echo Done.
-start chrome file://"%~dp0main.pdf"
